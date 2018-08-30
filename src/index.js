f274db1b371acce0c21709976481d9d6d68f11e4
Kevin Grieger <scuffmark@gmail.com>
2018-08-30 00:21:45 -0500

    added redux and react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import store, { history } from './store'
+import App from './containers/app'
 import registerServiceWorker from './registerServiceWorker';
 
 const root: ?Element = document.getElementById('root');
 
 if (root != null) {
-  ReactDOM.render(<App/>, root);
+  ReactDOM.render(  
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>, root);
   registerServiceWorker();
 }
